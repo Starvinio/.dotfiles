0a2587a5c572155d3ed3760a7d1d9c7bae61b1ab
starvinio <starvinio@proton.me>
2026-05-18 20:25:12 +0200

    Add zed settings

--- create_lns.sh
+++ create_lns.sh
@@ -4,13 +4,7 @@ set -e
 
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-mkdir -p ~/.config/{alacritty,nvim}
-mkdir -p ~/.config/nvim/colors
-
-# profile & bashrc
-ln -sfn "$DOTFILES_DIR/.profile" ~/.profile
-ln -sfn "$DOTFILES_DIR/.bashrc" ~/.bashrc
-echo "Linked .profile and .bashrc"
+mkdir -p ~/.config/{alacritty,nvim/colors,zed/themes}
 
 # alacritty
 ln -sfn "$DOTFILES_DIR/alacritty/alacritty.toml" ~/.config/alacritty/alacritty.toml
@@ -18,7 +12,14 @@ echo "Linked alacritty config"
 
 # neovim
 ln -sfn "$DOTFILES_DIR/nvim/init.lua" ~/.config/nvim/init.lua
-ln -sfn "$DOTFILES_DIR/nvim/colors/mentat2.lua" ~/.config/nvim/colors/mentat2.lua
+ln -sfn "$DOTFILES_DIR/nvim/colors/mentat.lua" ~/.config/nvim/colors/mentat.lua
 echo "Linked neovim config and colorscheme"
+
+# zed
+ln -sfn "$DOTFILES_DIR/zed/settings.json" ~/.config/zed/settings.json
+ln -sfn "$DOTFILES_DIR/zed/keymap.json" ~/.config/zed/keymap.json
+ln -sfn "$DOTFILES_DIR/zed/themes/mentat.json" ~/.config/zed/themes/mentat.json
+echo "Linked zed config and colorscheme"
+
 
 echo "All dotfiles successfully linked."
