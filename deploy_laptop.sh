15638a0a06288c811625049fe175feace189852d
Starvinio <starvinio@proton.me>
2026-08-10 08:58:03 +0200

    HUGE REWRITE: Modularize Sway Config; Add Laptop Deploy Script; Add Laptop Status; Add NixOS Configuration for Laptop;

--- deploy_laptop.sh
+++ deploy_laptop.sh
@@ -5,16 +5,21 @@ set -e
 DOTFILES="$(cd "$(dirname "$0")" && pwd)"
 COMMON="$DOTFILES/common"
 DESKTOP="$DOTFILES/desktop"
+LAPTOP="$DOTFILES/laptop"
 
 mkdir -vp ~/.config/{foot,,nvim/colors,sway,fuzzel,zathura,zed}
+
+# nixos 
+sudo ln -sfnv "$LAPTOP/nixos/configuration.nix" /etc/nixos/configuration.nix
+sudo ln -sfnv "$LAPTOP/nixos/hardware-configuration.nix" /etc/nixos/hardware-configuration.nix
 
 # sway
 ln -sfnv "$COMMON/sway/config" ~/.config/sway/config
 ln -sfnv "$COMMON/sway/colors.conf" ~/.config/sway/colors.conf
 ln -sfnv "$COMMON/sway/keybinds.conf" ~/.config/sway/keybinds.conf
 ln -sfnv "$COMMON/sway/bar.conf" ~/.config/sway/bar.conf
-ln -sfnv "$DESKTOP/sway/input.conf" ~/.config/sway/input.conf
-ln -sfnv "$DOTFILES/scripts/status_desktop.sh" ~/.config/sway/status.sh
+ln -sfnv "$LAPTOP/sway/input.conf" ~/.config/sway/input.conf
+ln -sfnv "$DOTFILES/scripts/status_laptop.sh" ~/.config/sway/status.sh
 
 # fuzzel
 ln -sfnv "$DESKTOP/fuzzel.ini" ~/.config/fuzzel/fuzzel.ini
@@ -24,7 +29,7 @@ ln -sfnv "$COMMON/bashrc" ~/.bashrc
 
 # alacritty
 ln -sfnv "$COMMON/theme.toml" ~/.config/alacritty/theme.toml
-ln -sfnv "$DESKTOP/alacritty.toml" ~/.config/alacritty/alacritty.toml
+ln -sfnv "$LAPTOP/alacritty.toml" ~/.config/alacritty/alacritty.toml
 
 # tmux 
 ln -sfnv "$COMMON/tmux.conf" ~/.tmux.conf
